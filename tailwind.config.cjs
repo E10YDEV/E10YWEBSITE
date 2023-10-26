29a573deb4831ced3f0f239d486af053593b93f3
Eloy123 <eloymartinez96@gmail.com>
2023-10-26 21:36:51 +0200

    ola

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
+      fontFamily: {
+				sans: ['Victor Mono Variable', ...defaultTheme.fontFamily.sans],
+			  },
       typography: (theme) => ({
         DEFAULT: {
           css: {
